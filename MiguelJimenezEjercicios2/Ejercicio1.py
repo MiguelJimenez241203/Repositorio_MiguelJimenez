b05259188a6e736b9511cd8ddd106d2c127c6f9a
MiguelJimenez241203 <142433029+MiguelJimenez241203@users.noreply.github.com>
2023-09-11 10:05:06 -0500

    Delete MiguelJimenezEjercicios2 directory

--- MiguelJimenezEjercicios2/Ejercicio1.py
+++
@@ -1,8 +0,0 @@
-L=(input('Digite una letra del abecedario '))
-if L in 'aeiou':
-    print('Es una vocal ')
-else:
-    if L in 'bcdfghjklmnñpqrstvwxyz':
-        print('Es una consonante ')
-    else:
-        print('El valor ingresado no es una letra del abecedario.')
